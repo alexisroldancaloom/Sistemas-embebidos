117c75a92a3984656afaa330c28b20a05b0afdc4
AlexisRoldanSanchez <alexisroldandanemi@gmail.com>
2024-02-23 22:32:31 -0600

    e1

--- practica2/ej7.py
+++ practica2/ej7.py
@@ -1,12 +1,53 @@
-from gpiozero import LEDBoard
+from gpiozero import LED
 from time import sleep
 from signal import pause
-leds = LEDBoard(5, 6, 13, 19, 26)
-leds.on()
+
+# Define los pines GPIO para cada LED
+pin_led1 = 5
+pin_led2 = 6
+pin_led3 = 13
+pin_led4 = 19
+pin_led5 = 26
+
+# Crea objetos LED individuales para cada LED
+led1 = LED(pin_led1)
+led2 = LED(pin_led2)
+led3 = LED(pin_led3)
+led4 = LED(pin_led4)
+led5 = LED(pin_led5)
+
+# Enciende todos los LEDs al principio
+led1.on()
+led2.on()
+led3.on()
+led4.on()
+led5.on()
+
 sleep(1)
-leds.off()
+
+# Apaga todos los LEDs después de un segundo
+led1.off()
+led2.off()
+led3.off()
+led4.off()
+led5.off()
+
 sleep(1)
-leds.value = (1, 0, 1, 0, 1)
+
+# Establece el estado de cada LED individualmente
+led1.value = 1
+led2.value = 0
+led3.value = 1
+led4.value = 0
+led5.value = 1
+
 sleep(1)
-leds.blink()
+
+# Hace parpadear todos los LEDs
+led1.blink()
+led2.blink()
+led3.blink()
+led4.blink()
+led5.blink()
+
 pause()
