8b1f05fe90162bf38a8e9cdc1f4eb9ed4518bf89
AlexisRoldanSanchez <alexisroldandanemi@gmail.com>
2024-05-13 21:16:01 -0600

    cerradura1

---
+++ proyecto/cerradura.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+import time
+
+# Configura el modo del pin
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17, GPIO.OUT)
+
+# Función para activar la cerradura
+def activar_cerradura():
+    GPIO.output(17, True)  # Enciende el relevador
+    time.sleep(5)          # Mantiene la cerradura abierta por 5 segundos
+    GPIO.output(17, False) # Apaga el relevador
+    time.sleep(5)          # Espera 5 segundos
+
+try:
+    while True:
+        activar_cerradura()
+except KeyboardInterrupt:
+    GPIO.cleanup()  # Limpia los ajustes de GPIO al salir
